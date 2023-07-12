584e653a6f7730d1a3225ff173d0456fe72ea55c
Francis Mathews <francismathews8530+@gmail.com>
2023-07-12 14:00:53 +0530

    script

---
+++ Ar avatar rScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AravatarrScript : MonoBehaviour
+{
+    public Vector3 a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(a*Time.deltaTime);  
+    }
+}
